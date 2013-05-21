7fec2ea34da6fd48002445f9c434c94ed258c02a
jerojohn <jeromyj@gmail.com>
2013-05-21 12:34:40 -0700

    add script to add sample user

---
+++ sampleuseradd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl 127.0.0.1:8080/users --data {"Action":"NEW", "Token":"thisismytoken", "Value":{"UUID":"11229324","FirstName":"Jeromy","LastName":"Johnson","Resume":"None Yet","Skills":["Golang","C","Genius"],"Major":"Computer Science","Minors":[],"Interests":["Females","Programming"],"Email":"me@jero.my"}}
